4f9ecf5b7a0a200ed62171f9d297255d0b723ea3
梅林 <2045206086@qq.com>
2025-02-26 18:46:22 +0800

    如此菠萝 出生尔 甚是猪鼻

--- src/main/java/net/p1nero/ss/gameassets/animations/ScreenSwordAnimations.java
+++ src/main/java/net/p1nero/ss/gameassets/animations/ScreenSwordAnimations.java
@@ -1,5 +1,6 @@
 package net.p1nero.ss.gameassets.animations;
 
+import net.p1nero.ss.entity.screen_sword.ScreenSwordArmature;
 import net.p1nero.ss.entity.vatansever_storm.VatanseverStormArmature;
 import net.p1nero.ss.gameassets.SwordSoaringArmatures;
 import yesman.epicfight.api.animation.property.AnimationProperty;
@@ -9,9 +10,9 @@ public class ScreenSwordAnimations {
     public static StaticAnimation SCREEN_SWORD_IDLE;
     public static StaticAnimation SCREEN_SWORD_1;
     public static void buildScreenSwordAnim() {
-        VatanseverStormArmature stormArmature = SwordSoaringArmatures.vatanseverStormArmature;
-        SCREEN_SWORD_IDLE = new StaticAnimation(true, "screen_sword/screen_sword_idle", stormArmature);
-        SCREEN_SWORD_1 = new StaticAnimation(true, "screen_sword/screen_sword_1", stormArmature);
+        ScreenSwordArmature screenSwordArmature = SwordSoaringArmatures.screenSwordArmature;
+        SCREEN_SWORD_IDLE = new StaticAnimation(true, "screen_sword/screen_sword_idle", screenSwordArmature);
+        SCREEN_SWORD_1 = new StaticAnimation(true, "screen_sword/screen_sword_1", screenSwordArmature);
     }
 
 }
