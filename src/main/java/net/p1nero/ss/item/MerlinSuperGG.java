e5c5545d18250d080cbda12e9494ce502b52f6d3
梅林 <2045206086@qq.com>
2025-04-12 16:42:35 +0800

    旋转修复

--- src/main/java/net/p1nero/ss/item/MerlinSuperGG.java
+++ src/main/java/net/p1nero/ss/item/MerlinSuperGG.java
@@ -56,7 +56,7 @@ public class MerlinSuperGG extends SwordItem {
             );
             if (entityPatch != null) {
                 entityPatch.playAnimation(
-                        WraithonAnimations.WRAITHON_3,
+                        WraithonAnimations.WRAITHON_ROTATE_R_160,
                         0.0F
                 );
             }
