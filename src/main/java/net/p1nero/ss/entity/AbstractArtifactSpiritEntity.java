69aca792e94136fee1f07ab674fcf71edd07bbce
p1nero <1070402601@qq.com>
2025-11-09 01:29:24 +0800

    防推，取消阴影绘制

--- src/main/java/net/p1nero/ss/entity/AbstractArtifactSpiritEntity.java
+++ src/main/java/net/p1nero/ss/entity/AbstractArtifactSpiritEntity.java
@@ -163,4 +163,8 @@ public abstract class AbstractArtifactSpiritEntity extends PathfinderMob impleme
         return false;
     }
 
+    @Override
+    public boolean isPushable() {
+        return false;
+    }
 }
